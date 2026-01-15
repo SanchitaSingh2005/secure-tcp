0d0c213bafad1d9478819c7bcaf6f378ab6c955e
SanchitaSingh2005 <23051373@kiit.ac.in>
2026-01-15 23:15:00 +0530

    Update client.py
    Added support for sending files securely to server, handling AES session encryption, and communicating with multi-client server.

--- client.py
+++ client.py
@@ -1,42 +1,44 @@
 import socket
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.fernet import Fernet
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.Random import get_random_bytes
+import os
 
-HOST = "127.0.0.1"
-PORT = 6000
+HOST = '127.0.0.1'
+PORT = 12345
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-# Receive public key
-public_pem = client.recv(1024)
-public_key = serialization.load_pem_public_key(public_pem)
+# Receive RSA public key
+public_key = RSA.import_key(client.recv(450))
+cipher_rsa = PKCS1_OAEP.new(public_key)
 
-# Generate AES key
-aes_key = Fernet.generate_key()
-cipher = Fernet(aes_key)
+# AES session key
+session_key = get_random_bytes(16)
+client.send(cipher_rsa.encrypt(session_key))
 
-# Encrypt AES key with RSA
-encrypted_aes = public_key.encrypt(
-    aes_key,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
+while True:
+    msg = input("Enter message or 'SEND filename': ")
+    if msg.lower() == 'exit':
+        break
 
-client.send(encrypted_aes)
-
-# Send encrypted message
-message = cipher.encrypt(b"Hello with hybrid encryption")
-client.send(message)
-
-# Receive reply
-encrypted_reply = client.recv(1024)
-reply = cipher.decrypt(encrypted_reply)
-
-print("Server reply:", reply.decode())
+    if msg.startswith("SEND "):
+        filename = msg.split(" ")[1]
+        if not os.path.exists(filename):
+            print("File not found!")
+            continue
+        filesize = os.path.getsize(filename)
+        client.send(AES.new(session_key, AES.MODE_EAX).encrypt(msg.encode()))  # command encrypted
+        client.send(f"{filesize:<16}".encode())  # send file size
+        with open(filename, "rb") as f:
+            client.sendall(f.read())
+        print(f"File {filename} sent.")
+    else:
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext, tag = cipher_aes.encrypt_and_digest(msg.encode())
+        client.send(cipher_aes.nonce)
+        client.send(tag)
+        client.send(ciphertext)
 
 client.close()
